8b365df6a2096e4f5aee8b269fba729df5cefff4
PiratKrabe <Link_Ex@i.ua>
2017-06-12 09:56:42 +0300

    Home Work 1.1

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by Linkin on 12.06.2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter Your name: ");
+        String n = reader.next();
+        System.out.println(" Hello, " + n);
+    }
+}
